7636ec8a24c7def988056ded69b3639afe306197
Thesi <thesi.automation2019@gmail.com>
2021-07-05 10:22:37 +0700

    handle actuator topics

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ mqtt_client = mqtt.Client(client_id="SQL_Handle", clean_session=True)
 # Specify callback function
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
+
+# Callback for every topic
 mqtt_client.message_callback_add("+/mq2", mq2_handle)
 mqtt_client.message_callback_add("+/bmp180", bmp180_handle)
 mqtt_client.message_callback_add("+/si7021", si7021_handle)
+mqtt_client.message_callback_add("+/light1", light1_handle())
+mqtt_client.message_callback_add("+/light2", light2_handle())
+mqtt_client.message_callback_add("+/door", door_handle)
+mqtt_client.message_callback_add("+/fan", fan_handle)
 
 # Establish a connection
 mqtt_client.connect(host="192.168.0.101", port=1883, keepalive=60)
--- models/handle_mqtt.py
+++ models/handle_mqtt.py
@@ -5,18 +5,17 @@ from models.model_sqlalchemy import *
 # Connection success callback
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {str(rc)}")
-    client.subscribe("sensor/#")
+    client.subscribe("#")
 
 
 # Message receiving callback
-
-
 def on_message(client, userdata, msg):
     # payload = json.loads(msg.payload.decode('ascii')[:-1])
     # print(f"{msg.topic} {payload}")
     pass
 
 
+# Sensors
 def mq2_handle(client, userdata, msg):
     payload = json.loads(msg.payload.decode('ascii')[:-1])
     Sensors.add(7, payload, datetime.now())
@@ -30,3 +29,24 @@ def bmp180_handle(client, userdata, msg):
 def si7021_handle(client, userdata, msg):
     payload = json.loads(msg.payload.decode('ascii')[:-1])
     Sensors.add(11, payload, datetime.now())
+
+
+# Actuators
+def light1_handle(client, userdata, msg):
+    payload = str(msg.payload.decode('ascii')[:-1])
+    Actuators.add(1, payload, datetime.now())
+
+
+def light2_handle(client, userdata, msg):
+    payload = str(msg.payload.decode('ascii')[:-1])
+    Actuators.add(2, payload, datetime.now())
+
+
+def door_handle(client, userdata, msg):
+    payload = str(msg.payload.decode('ascii')[:-1])
+    Actuators.add(3, payload, datetime.now())
+
+
+def fan_handle(client, userdata, msg):
+    payload = str(msg.payload.decode('ascii')[:-1])
+    Actuators.add(4, payload, datetime.now())
